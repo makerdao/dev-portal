0a71df55136bfe02f86551e410dba9cf749b6860
sirromdev <sirromdev@gmail.com>
2019-05-07 13:35:16 +0100

    removed comments

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -3,14 +3,9 @@ const ServerStyleSheet = require('styled-components').ServerStyleSheet
 const renderToStaticMarkup = require('react-dom/server').renderToStaticMarkup
 const fs = require('fs');
 
-const { makerTeal, makerOrange, daiYellow, heading, linkBlue } = require("@makerdao/ui-components-core").themeLight.colors;
-
-// List of projects/orgs using your project for the users page.
 const users = [
   {
     caption: 'Maker',
-    // You will need to prepend the image path with your baseUrl
-    // if it is not '/', like: '/test-site/img/docusaurus.svg'.
     image: '/img/maker.svg',
     infoLink: 'https://www.makerdao.com',
     pinned: true,
@@ -18,87 +13,38 @@ const users = [
 ];
 
 const siteConfig = {
-  title: 'MakerDAO Developer Portal', // Title for your website.
+  title: 'MakerDAO Developer Portal',
   tagline: 'Live free or Dai hard!',
-  url: 'https://developers.makerdao.com', // Your website URL
-  baseUrl: '/', // Base URL for your project */
-  // For github.io type URLs, you would set the url and baseUrl like:
-  //   url: 'https://facebook.github.io',
-  //   baseUrl: '/test-site/',
-
-  // Used for publishing and more
+  url: 'https://developers.makerdao.com',
+  baseUrl: '/',
   projectName: 'Maker DevPortal',
   organizationName: 'facebook',
-  // For top-level user or org sites, the organization is still the same.
-  // e.g., for the https://JoelMarcey.github.io site, it would be set like...
-  //   organizationName: 'JoelMarcey'
-
-  // For no header links in the top nav bar -> headerLinks: [],
   headerLinks: [
     {doc: 'getting-started/introduction', label: 'Docs'},
     {doc: 'learn/introduction', label: 'Learn'},
     {page: 'help', label: 'Products'},
     {href: 'https://blog.makerdao.com/', label: 'Community'}
   ],
-
   docPaths: JSON.parse(fs.readFileSync('sidebars.json')),
-  // If you have users set above, you add it here:
   users,
-
-  /* path to images for header/footer */
   headerIcon: 'img/maker.svg',
   footerIcon: 'img/maker.svg',
   favicon: 'img/favicon.png',
-
-  /* Colors for website */
   colors: {
     primaryColor: heading,
     secondaryColor: makerOrange,
   },
-
-  /* Custom fonts for website */
-  /*
-  fonts: {
-    myFont: [
-      "Times New Roman",
-      "Serif"
-    ],
-    myOtherFont: [
-      "-apple-system",
-      "system-ui"
-    ]
-  },
-  */
-
-  // This copyright info is used in /core/Footer.js and blog RSS/Atom feeds.
   copyright: `Copyright © ${new Date().getFullYear()} MakerDAO`,
-
   highlight: {
-    // Highlight.js theme to use for syntax highlighting in code blocks.
     theme: 'default',
   },
-
-  // Add custom scripts here that would be placed in <script> tags.
   scripts: ['https://buttons.github.io/buttons.js'],
-
-  // On page navigation for the current documentation page.
   onPageNav: 'separate',
-  // No .html extensions for paths.
   cleanUrl: true,
-
-  // Open Graph and Twitter card images.
   ogImage: 'img/docusaurus.png',
   twitterImage: 'img/docusaurus.png',
-
-  // Show documentation's last contributor's name.
   enableUpdateBy: true,
-
-  // Show documentation's last update time.
   enableUpdateTime: true,
-
-  // You may provide arbitrary config keys to be used as needed by your
-  // template. For example, if you need your repo's URL...
-  //   repoUrl: 'https://github.com/facebook/test-site',
   renderToString: element => {
     const sheet = new ServerStyleSheet()
     const collected = sheet.collectStyles(
@@ -118,7 +64,6 @@ const siteConfig = {
       ${html.slice(insertStylesAt)}
     `.trim()
   },
-
 }
 
 module.exports = siteConfig;
